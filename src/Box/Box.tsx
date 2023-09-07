b8e94c21b91a3c25ae52c29fc87f43e815dafff4
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-09-07 21:19:00 +0300

    feat: added Box component

---
+++ src/Box/Box.tsx
@@ -0,0 +1,15 @@
+import {View} from 'react-native';
+
+export const Box = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+interface BoxPropsType {
+    bgr: string;
+    width: number;
+    height?: number;
+}
